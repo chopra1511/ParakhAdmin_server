f4f9368a5b227d15c06cf3f9b4ba39c2f5791de8
Rahul Chopra <scorpio.sk98@gmail.com>
2024-09-19 18:58:32 +0530

    updated changes

--- routes/order.js
+++ routes/order.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/get-orders", getOrders);
 
-router.post("/order-details/", getOrderDetails);
+router.post("/order-details", getOrderDetails);
 
 router.post("/order-tracking", updateTracking);
 
